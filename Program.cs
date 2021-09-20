d3dbf2c7b1115faec9bcd7d0f3a7a3598507c986
funk <68274507+1ceman420@users.noreply.github.com>
2021-09-20 20:09:59 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace remainder_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to my remainder calculator");
+            Console.WriteLine("input number 1");
+            int number1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("input number 2");
+            int number2 = Int32.Parse(Console.ReadLine());
+            int result1 = number1 / number2;
+            int result2 = number1 % number2;
+            Console.WriteLine("The answer is: {0} remainder {1} ",result1,result2);
+            Console.ReadLine();
+        }
+    }
+}
